1c92d55bfcb7ca70e69b058896fc51449e5a880f
Maksym <legonner@gmail.com>
2021-11-30 18:22:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(3*3=='9');
+
+const isChecked = true,
+      isClose = true;
+console.log(isChecked && isClose)
